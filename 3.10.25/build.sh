9f0d7c36ebe1cc772dddf772b539fd6e7a8b431b
xuechun <609022045@qq.com>
2023-07-23 08:33:16 +0800

    namespace
    namespace

--- 3.10.25/build.sh
+++ 3.10.25/build.sh
@@ -1,3 +1,3 @@
 wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.12.0/rabbitmq_delayed_message_exchange-3.12.0.ez
-docker build -t rabbitmq:3.10.25-management-delayed .
+docker build -t shshdxk/rabbitmq:3.10.25-management-delayed .
 
