8fd6c94c8aa9da1ed2d75e9f044292a20a3b630c
xuechun <609022045@qq.com>
2023-08-17 12:37:10 +0800

    3.8.2
    3.8.2

---
+++ 3.8.2/build.sh
@@ -0,0 +1,2 @@
+wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.8.0/rabbitmq_delayed_message_exchange-3.8.0.ez
+docker build -t shshdxk/rabbitmq-management-delayed:3.8.2 .
