3264fd51bf13e0b6d1b4972dc09700067e858d12
shshdxk <609022045@qq.com>
2024-11-05 10:30:55 +0800

    添加3.12.14
    添加3.12.14

---
+++ 3.12/3.12.14/build.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.12.0/rabbitmq_delayed_message_exchange-3.12.0.ez
+docker build -t shshdxk/rabbitmq-management-delayed:3.12.14 .
+
