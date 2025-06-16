f14be2b6c8f0374464212ab5dabe7c7374fae709
shshdxk <609022045@qq.com>
2025-06-16 14:52:14 +0800

    4.1.1
    4.1.1

---
+++ 4.1/4.1.1/build.sh
@@ -0,0 +1,2 @@
+wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v4.1.0/rabbitmq_delayed_message_exchange-4.1.0.ez
+docker build -t shshdxk/rabbitmq-management-delayed:4.1.1 .
